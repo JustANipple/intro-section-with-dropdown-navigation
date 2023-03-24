5f90670e2c150103e2535d90fff656225645c2ba
JustANipple <prandisamuel99@gmail.com>
2023-03-24 10:53:02 +0100

    Merge branch 'master' of https://github.com/JustANipple/intro-section-with-dropdown-navigation

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const menuIcon = document.querySelector(".menu_icon");
+const darkLayer = document.querySelector(".menu_back_layer");
+const menu = document.querySelector(".menu");
+
+menuIcon.addEventListener("click", (event) => {
+    if(!menuIcon.getAttribute("src").includes("close")) {
+        menuIcon.setAttribute("src", "images/icon-close-menu.svg");
+        darkLayer.style.display = "block";
+        menu.style.display = "block";
+    } else {
+        menuIcon.setAttribute("src", "images/icon-menu.svg");
+        darkLayer.style.display = "none";
+        menu.style.display = "none";
+    }
+});
