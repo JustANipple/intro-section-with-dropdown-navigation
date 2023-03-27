9bc8674c7a2e374f3d64f73557fba1cf52dfc9fc
JustANipple <prandisamuel99@gmail.com>
2023-03-27 12:27:49 +0200

    new file:   design/screenshot.png 	modified:   index.html 	modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	new file:   styles/scss/components/_content.scss 	modified:   styles/scss/components/_nav.scss 	modified:   styles/scss/main.scss

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,17 @@ const menuIcon = document.querySelector(".nav_menu_icon");
 const darkLayer = document.querySelector(".nav_dark_layer");
 const menu = document.querySelector(".nav_menu");
 const listHeaders = document.querySelectorAll(".list_header");
+const button = document.querySelector(".content_button");
+const listElems = document.querySelectorAll("li");
 
 menuIcon.addEventListener("click", (event) => {
     if(!menuIcon.getAttribute("src").includes("close")) {
         menuIcon.setAttribute("src", "images/icon-close-menu.svg");
-        darkLayer.style.opacity = "80%";
+        darkLayer.style.display = "block";
         menu.style.display = "block";
     } else {
         menuIcon.setAttribute("src", "images/icon-menu.svg");
-        darkLayer.style.opacity = "0";
+        darkLayer.style.display = "none";
         menu.style.display = "none";
     }
 });
@@ -31,4 +33,31 @@ function changeArrow() {
         list.style.animation = "collapseList .5s forwards";
         list.style.transition = "all .5s ease-in-out";
     }
+}
+
+button.addEventListener("mouseover", (event) => {
+    button.style.backgroundColor = "transparent";
+    button.style.color = "black";
+    button.style.transition = "all .5s ease-in-out";
+});
+
+button.addEventListener("mouseout", (event) => {
+    button.style.backgroundColor = "black";
+    button.style.color = "white";
+    button.style.transition = "all .5s ease-in-out";
+});
+
+for(elem of listElems) {
+    elem.addEventListener("mouseover", zoomElem);
+    elem.addEventListener("mouseout", dezoomElem);
+}
+
+function zoomElem() {
+    this.style.transform = "scale(1.1)";
+    this.style.transition = "transform .5s ease-in-out";
+}
+
+function dezoomElem() {
+    this.style.transform = "scale(1)";
+    this.style.transition = "transform .5s ease-in-out";
 }
