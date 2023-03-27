ffde1d5efdd63394b2c602fcf74e77ba7614de80
JustANipple <prandisamuel99@gmail.com>
2023-03-27 10:55:30 +0200

    modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_nav.scss 	modified:   styles/scss/main.scss

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,34 @@
 const menuIcon = document.querySelector(".nav_menu_icon");
 const darkLayer = document.querySelector(".nav_dark_layer");
 const menu = document.querySelector(".nav_menu");
-const arrow = document.querySelectorAll(".header_arrow");
+const listHeaders = document.querySelectorAll(".list_header");
 
 menuIcon.addEventListener("click", (event) => {
     if(!menuIcon.getAttribute("src").includes("close")) {
         menuIcon.setAttribute("src", "images/icon-close-menu.svg");
-        darkLayer.style.display = "block";
-        menu.style.display = "grid";
+        darkLayer.style.opacity = "80%";
+        menu.style.display = "block";
     } else {
         menuIcon.setAttribute("src", "images/icon-menu.svg");
-        darkLayer.style.display = "none";
+        darkLayer.style.opacity = "0";
         menu.style.display = "none";
     }
 });
 
 
-for(arr of arrow) {
+for(arr of listHeaders) {
     arr.addEventListener("click", changeArrow);
 }
 function changeArrow() {
-    const list = this.parentNode.parentNode.children[1];
-    if(this.getAttribute("src").includes("down")) {
-        this.setAttribute("src", "images/icon-arrow-up.svg");
-        list.style.display = "block";
-        list.style.animation = "growDown .5s ease-in-out";
+    const list = this.parentNode.children[1];
+    if(this.children[1].getAttribute("src").includes("down")) {
+        this.children[1].setAttribute("src", "images/icon-arrow-up.svg");
+        list.style.animation = "dropList .5s forwards";
+        list.style.transition = "all .5s ease-in-out";
 
     } else {
-        this.setAttribute("src", "images/icon-arrow-down.svg");
-        list.style.animation = "growUp .5s ease-in-out";
+        this.children[1].setAttribute("src", "images/icon-arrow-down.svg");
+        list.style.animation = "collapseList .5s forwards";
+        list.style.transition = "all .5s ease-in-out";
     }
 }
